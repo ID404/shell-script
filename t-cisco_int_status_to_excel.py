531c68e6667f36dbbb3c72f7472d44b486376494
ID404 <ljc8903@126.com>
2023-09-20 11:43:54 +0800

    Update t-cisco_int_status_to_excel.py

--- t-cisco_int_status_to_excel.py
+++ t-cisco_int_status_to_excel.py
@@ -18,21 +18,56 @@ if len(sys.argv) == 1:
     print("此程序主要用于将思科交换机show int status 输出至log文本的信息复制至excel中\n请在", sys.argv[0], "后输入文件名")
     sys.exit(1)
 else:
-    print("请确保用作分析的文本只含有show interface status信息,若包含其它配置内容则输出有误")
+    print("请确保用作分析的文本只含有show interface status信息,并包含标题行，若包含其它配置内容则输出有误")
     inputfile = sys.argv[1]
 
-# 清理空行和标题行
+
+# 清理空行并检查是否包含标题行
 with open(inputfile, 'r') as file:
     lines = file.readlines()
-
-# 清理空行和同时包含 'Port' 和 'Name' 的行
-lines = [line.strip() for line in lines if line.strip() and ('Port' not in line or 'Name' not in line)]
-
+# 清理空行
+lines = [line.strip() for line in lines if line.strip()]
+# 检查首行是否包含 'Port' 和 'Name'
+if not lines[0].startswith('Port') or 'Name' not in lines[0]:
+    print("警告：首行不存在标题，请同时将标题行复制至分析文件。程序退出。")
+    exit()
 with open(inputfile, 'w') as file:
     file.write('\n'.join(lines))
 
-# 关闭文件
-file.close()
+
+
+#提取Port", "Name", "Status", "Vlan", "Duplex所在的列
+def find_word_positions(filename):
+    positions = {}
+    with open(filename, 'r') as file:
+        line = file.readline()
+        words = line.split(" ")
+        pos = 1
+        for word in words:
+            if word in ["Port", "Name", "Status", "Vlan", "Duplex","Speed"]:
+                positions[word] = pos
+            pos += len(word) + 1
+    return positions
+
+# 使用函数并将结果赋值给变量
+word_positions = find_word_positions(inputfile)
+
+# 分别赋值给变量并在终端输出
+Port = word_positions['Port']
+Name = word_positions['Name']
+Status = word_positions['Status']
+Vlan = word_positions['Vlan']
+Duplex = word_positions['Duplex']
+Speed = word_positions['Speed']
+
+print("Port:", Port-1)
+print("Name:", Name-1)
+print("Status:", Status-1)
+print("Vlan:", Vlan-1)
+print("Duplex:", Duplex-1)
+print("Speed:", Speed-1)
+
+
 
 # 分列处理,将处理的文本每一列提取至一个文本文件
 with open(inputfile, 'r') as file:
@@ -42,12 +77,13 @@ with open(inputfile, 'r') as file:
 #注意!!!!!!!!!!!!!!!!!
 #注意!!!!!!!!!!!!!!!!!
 #需要根据实际文件修改截断的位置，不同型号的交换机截断位置可能不一样
-temp_port = [line[0:13].strip() for line in lines]
-temp_name = [line[13:32].strip() for line in lines]
-temp_status = [line[32:45].strip() for line in lines]
-temp_vlan = [line[45:56].strip() for line in lines]
-temp_duplex = [line[56:63].strip() for line in lines]
-temp_speed_type = [line[63:].strip() for line in lines]
+#待处理，提取标题所在列，不需要人工判断
+temp_port = [line[0:Name-1].strip() for line in lines]
+temp_name = [line[Name-1:Status-1].strip() for line in lines]
+temp_status = [line[Status-1:Vlan-1].strip() for line in lines]
+temp_vlan = [line[Vlan-1:Duplex-1].strip() for line in lines]
+temp_duplex = [line[Duplex-1:Speed-2].strip() for line in lines]
+temp_speed_type = [line[Speed-2:].strip() for line in lines]
 
 with open('./temp_port.txt', 'w') as file:
     file.write('\n'.join(temp_port))
@@ -75,7 +111,7 @@ file_name = f"result_{current_time}.xlsx"
 
 # 打开xlsx文件以追加数据
 workbook = xlsxwriter.Workbook(file_name)
-worksheet = workbook.add_worksheet()
+worksheet = workbook.add_worksheet("show interface status")
 
 # 定义输入文件列表
 input_files = ['temp_port.txt', 'temp_name.txt', 'temp_status.txt', 'temp_vlan.txt', 'temp_duplex.txt','temp_Speed_Type.txt']
@@ -83,10 +119,11 @@ input_files = ['temp_port.txt', 'temp_name.txt', 'temp_status.txt', 'temp_vlan.t
 # 冻结首行
 worksheet.freeze_panes(1, 0)
 
+
 # 写入xlsx第一行标题
-titles = ['port', 'name', 'status', 'vlan', 'duplex', 'speed_type']
-for col, title in enumerate(titles):
-    worksheet.write(0, col, title)
+#titles = ['port', 'name', 'status', 'vlan', 'duplex', 'speed_type']
+#for col, title in enumerate(titles):
+#    worksheet.write(0, col, title)
 
 
 # 设置单元格格式
@@ -103,17 +140,17 @@ for col, input_file in enumerate(input_files):
 
         for row, line in enumerate(lines):
             line = line.strip()
-            worksheet.write(row + 1, col, line)
+            worksheet.write(row, col, line)
 
             # 设置单元格颜色
             if line == 'connected':
-                worksheet.write(row + 1, col, line, cell_format_green)
+                worksheet.write(row, col, line, cell_format_green)
             elif line == 'notconnect':
-                worksheet.write(row + 1, col, line, cell_format_blue)
+                worksheet.write(row, col, line, cell_format_blue)
             elif line == 'disabled':
-                worksheet.write(row + 1, col, line, cell_format_red)
+                worksheet.write(row, col, line, cell_format_red)
             elif line == 'suspended':
-                worksheet.write(row + 1, col, line, cell_format_yellow)
+                worksheet.write(row, col, line, cell_format_yellow)
 
         # 获取每一列的最大宽度
         column_widths.append(max(len(line) for line in lines))
