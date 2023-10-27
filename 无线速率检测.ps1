693403457f3f15a4b14daaa32ed2c1444e5d6d0b
ID404 <ljc8903@126.com>
2023-10-27 11:22:55 +0800

    Delete 无线速率检测.ps1

--- 无线速率检测.ps1
+++
@@ -1,8 +0,0 @@
-
-while ($true) {
-    $interface = Get-NetAdapter | Where-Object {$_.InterfaceDescription -like "*Wireless*"}
-    $speed = $interface | Select-Object -ExpandProperty LinkSpeed
-    $current_time = Get-Date -Format "yyyy-MM-dd HH:mm:ss"
-    Write-Output ("{0} - 当前 WiFi 速率为：{1} " -f $current_time, $speed)
-    Start-Sleep -Seconds 1
-}
